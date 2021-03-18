0a99edefc3e068ad81d6d7f21e11d930cfa89bd1
johansonfelix <felix.johanson@gmail.com>
2021-03-17 21:58:50 -0400

    Changed SOAP service.

--- src/main/java/com/soap/service/LoggerImpl.java
+++ src/main/java/com/soap/service/LoggerImpl.java
@@ -1,6 +1,7 @@
 package com.soap.service;
 
 import DAO.DatabaseManager;
+import com.Albums;
 import exceptions.RepException;
 import pojo.LogEntry;
 
@@ -10,23 +11,23 @@ import java.util.ArrayList;
 @WebService(name ="Logger", endpointInterface = "com.soap.service.Logger")
 public class LoggerImpl implements Logger {
 
-    DatabaseManager db = DatabaseManager.getInstance();
+    Albums albums = Albums.getAlbumsInstance();
 
     @Override
     public ArrayList<LogEntry> getChangeLogs() throws RepException {
 
-        ArrayList<LogEntry> logs = db.getAllLogEntries();
+        ArrayList<LogEntry> logs = albums.getLogs();
         if(logs.size()==0) {
             System.out.println("REPEXCEPTION: NO LOG ENTRIES.");
-            throw new RepException();
-            //throw new com.soap.service.RepException("NO LOG ENTRIES");
+            throw new RepException("REPEXCEPTION: NO LOG ENTRIES.");
+
         }
         else {
             System.out.println("Returning Change Logs...");
-            return db.getAllLogEntries();
+            return logs;
         }
     }
 
     @Override
-    public void clearLogs() throws RepException{ throw new RepException("The Method is not yet supported.");}
+    public void clearLogs() throws RepException{ System.out.println("REPEXCEPTION: The Method is not yet supported.");throw new RepException("The Method is not yet supported.");}
 }
